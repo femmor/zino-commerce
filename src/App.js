937dd3c2e1d5ddfe8190a4a678b99db7b22537b2
Emmanuel Egomson <fegomson@gmail.com>
2022-07-15 14:17:20 -0500

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/home/Home';
+import Admin from './pages/admin/Admin';
+import OrderHistory from './pages/orderHistory/OrderHistory';
+import Contact from './pages/contact/Contact';
+import Cart from './pages/cart/Cart';
+import NotFound from './pages/not-found/NotFound';
+
+// Components
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Zino Commerce</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/order-history" element={<OrderHistory />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
---
+++ src/pages/not-found/NotFound.js
@@ -0,0 +1,10 @@
+import './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Error page</h1>
+    </div>
+  );
+};
+export default NotFound;
