add353badd75cb29aee97e0b807e84395e7cc5eb
Emmanuel Egomson <fegomson@gmail.com>
2022-07-15 14:39:52 -0500

    Create header logo section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,17 @@
-import './Header.module.scss';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
 
 const Header = () => {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+  return <header className={styles.header}>{logo}</header>;
 };
 export default Header;
