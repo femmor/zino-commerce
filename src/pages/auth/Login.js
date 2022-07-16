a42aff35d3ea6a88227c41d98a301ee17f4cc184
Emmanuel Egomson <fegomson@gmail.com>
2022-07-16 13:04:23 -0500

    Create the card component

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import loginImg from '../../assets/login.png';
 import styles from './Auth.module.scss';
 import { FaGoogle } from 'react-icons/fa';
+import Card from '../../components/card/Card';
 
 const Login = () => {
   return (
@@ -9,41 +10,43 @@ const Login = () => {
       <div className={styles.img}>
         <img src={loginImg} alt="login" width={400} />
       </div>
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <form>
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button className="--btn --btn-primary --btn-block" type="submit">
-            Login
-          </button>
-          <div>
-            <Link to="/reset-password">Forgot password?</Link>
-          </div>
-          <p>-- or --</p>
-        </form>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button className="--btn --btn-primary --btn-block" type="submit">
+              Login
+            </button>
+            <div>
+              <Link to="/reset-password">Forgot password?</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
 
-        <button className="--btn --btn-danger --btn-block" type="submit">
-          <FaGoogle
-            color="#fff"
-            style={{
-              marginRight: '5px',
-            }}
-          />
-          Login with Google
-        </button>
-        <span className={styles.register}>
-          Don't have an account?
-          <Link
-            to="/register"
-            style={{
-              marginLeft: '5px',
-            }}
-          >
-            Register
-          </Link>
-        </span>
-      </div>
+          <button className="--btn --btn-danger --btn-block" type="submit">
+            <FaGoogle
+              color="#fff"
+              style={{
+                marginRight: '5px',
+              }}
+            />
+            Login with Google
+          </button>
+          <span className={styles.register}>
+            Don't have an account?
+            <Link
+              to="/register"
+              style={{
+                marginLeft: '5px',
+              }}
+            >
+              Register
+            </Link>
+          </span>
+        </div>
+      </Card>
     </section>
   );
 };
