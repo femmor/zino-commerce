e887d327d83f3e75f68625de3978a41b84d8a263
Emmanuel Egomson <fegomson@gmail.com>
2022-07-18 01:43:13 -0500

    Register logic and react-toastify

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,18 +1,46 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import registerImg from '../../assets/register.png';
 import styles from './Auth.module.scss';
 import Card from '../../components/card/Card';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [cPassword, setCPassword] = useState('');
+
+  const registerUser = e => {
+    e.preventDefault();
+    console.log(email, password, cPassword);
+  };
+
   return (
     <section className={`container ${styles.auth}`}>
       <Card>
         <div className={styles.form}>
           <h2>Register</h2>
-          <form>
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder="Confirm Password" required />
+          <form onSubmit={registerUser}>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              required
+              value={cPassword}
+              onChange={e => setCPassword(e.target.value)}
+            />
             <button className="--btn --btn-primary --btn-block" type="submit">
               Register
             </button>
