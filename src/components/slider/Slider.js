4081060155aa6fa13d49946032c3897f17e2ce4d
Emmanuel Egomson <fegomson@gmail.com>
2022-07-19 01:23:53 -0500

    Slider logic

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -7,10 +7,34 @@ import './Slider.scss';
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const handleNext = () => {
+    if (currentSlide === sliderData.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(sliderData.length - 1);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
   return (
     <div className="slider">
-      <FaArrowCircleLeft className="arrow prev" size={20} />
-      <FaArrowCircleRight className="arrow next" size={20} />
+      <FaArrowCircleLeft
+        className="arrow prev"
+        size={20}
+        onClick={handlePrev}
+      />
+      <FaArrowCircleRight
+        className="arrow next"
+        size={20}
+        onClick={handleNext}
+      />
 
       {sliderData.map((slide, index) => {
         const { image, heading, desc } = slide;
