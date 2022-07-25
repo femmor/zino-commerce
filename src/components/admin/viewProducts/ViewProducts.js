8c18c21e816f1b7eaea1aea7c790dd766a801952
Emmanuel Egomson <fegomson@gmail.com>
2022-07-25 16:41:27 -0500

    Display products on the page

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -11,6 +11,9 @@ import styles from './ViewProducts.module.scss';
 import Loader from '../../../components/loader/Loader';
 import { toast } from 'react-toastify';
 
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -46,7 +49,55 @@ const ViewProducts = () => {
   return (
     <>
       {isLoading && <Loader />}
-      <div className={styles.products}></div>
+      <div className={styles.table}>
+        <h2>All Products</h2>
+
+        {products.length === 0 ? (
+          <p>No product found...</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            {products.map((product, i) => {
+              const { id, name, price, imageURL, category } = product;
+              return (
+                <tbody key={id}>
+                  <tr>
+                    <td>{i + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{
+                          width: '100px',
+                        }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>${price}</td>
+                    <td>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt size={19} color="red" />
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        )}
+      </div>
     </>
   );
 };
