7dbc836c27eb3684aefe98fae96c95a0cb3fc1fa
Emmanuel Egomson <fegomson@gmail.com>
2022-07-27 16:16:52 -0500

    Use params to add and edit product

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -132,7 +132,7 @@ const ViewProducts = () => {
                     <td>{category}</td>
                     <td>${price}</td>
                     <td className={styles.icons}>
-                      <Link to="/admin/add-product">
+                      <Link to={`/admin/add-product/${id}`}>
                         <FaEdit size={20} color="green" />
                       </Link>
                       &nbsp;
--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -17,7 +17,7 @@ const Admin = () => {
       <div className={styles.content}>
         <Routes>
           <Route path="home" element={<Home />} />
-          <Route path="add-product" element={<AddProduct />} />
+          <Route path="add-product/:id" element={<AddProduct />} />
           <Route path="orders" element={<Orders />} />
           <Route path="view-products" element={<ViewProducts />} />
         </Routes>
